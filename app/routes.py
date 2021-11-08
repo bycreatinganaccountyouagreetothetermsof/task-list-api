9b49ba4cbc15be56b0fb43312e9adca712e41be1
lain <bycreatinganaccountyouagreetothetermsof@users.noreply.github.com>
2021-11-07 16:43:28 -0800

    single task route

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,19 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, request
 
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+
+@tasks_bp.route("", methods=["GET", "POST"])
+def handle_tasks():
+    if request.method == "GET":
+        return jsonify([task.to_dict() for task in Task.query.all()])
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_task = Task(
+            title=request_body["title"], description=request_body["description"]
+        )
+        db.session.add(new_task)
+        db.session.commit()
+        return f"Task {new_task.title} successfully created", 201
