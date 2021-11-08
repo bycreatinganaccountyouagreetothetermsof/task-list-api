15d27dcf50dc76948aab4b6b417b441de6053528
lain <bycreatinganaccountyouagreetothetermsof@users.noreply.github.com>
2021-11-08 11:46:06 -0800

    doc's trangs

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,14 @@
+"""
+# project notes
+
+wave 4 incomplete. may implement on mastodon instead of slack, later
+
+some routes are genericized and switch on dict provided by select_model and blueprint name
+
+as much as possible, json rendering is kept out of models and performed in route functions, aside from the typical to_dict
+
+"""
+
 from app import db
 from app.models.task import Task
 from app.models.goal import Goal
@@ -14,6 +25,19 @@ select_model = {"task": Task, "goal": Goal}
 @tasks_bp.route("", methods=["GET"])
 @goals_bp.route("", methods=["GET"])
 def get_all_items():
+    """
+    This route will return all records of a model.
+    ---
+    parameters:
+        - name: sort
+            in: path
+            type: string
+            required: false
+            description: sort by title asc or desc
+    responses:
+        200:
+            description: Successful response containing all records of model. May be empty.
+    """
     model = select_model[request.blueprint]
     sort = {"asc": model.title.asc(), "desc": model.title.desc()}.get(
         request.args.get("sort")
@@ -25,6 +49,15 @@ def get_all_items():
 @tasks_bp.route("", methods=["POST"])
 @goals_bp.route("", methods=["POST"])
 def post_new_item():
+    """
+    This route will accept a new record via POST.
+    ---
+    responses:
+        200:
+            description: Successfully added new record.
+        400:
+            description: Post body did not include all fields required to create record.
+    """
     model = select_model[request.blueprint]
     label = request.blueprint
     request_body = request.get_json()
@@ -39,6 +72,15 @@ def post_new_item():
 @tasks_bp.route("/<item_id>", methods=["DELETE", "PUT", "GET"])
 @goals_bp.route("/<item_id>", methods=["DELETE", "PUT", "GET"])
 def single_item(item_id):
+    """
+    This route will read or modify a record.
+    ---
+    responses:
+        200:
+            description: Successfully read or modified record.
+        404:
+            description: Requested record does not exist.
+    """
     model = select_model[request.blueprint]
     label = request.blueprint
     item = model.query.get_or_404(item_id)
@@ -57,6 +99,15 @@ def single_item(item_id):
 
 @tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
 def complete_task(task_id):
+    """
+    This route will mark a task complete.
+    ---
+    responses:
+        200:
+            description: Successfully completed task.
+        404:
+            description: Requested task does not exist.
+    """
     task = Task.query.get_or_404(task_id)
     task.completed_at = datetime.now()
     db.session.commit()
@@ -65,6 +116,15 @@ def complete_task(task_id):
 
 @tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
 def incomplete_task(task_id):
+    """
+    This route will mark a task incomplete.
+    ---
+    responses:
+        200:
+            description: Successfully un-completed task.
+        404:
+            description: Requested task does not exist.
+    """
     task = Task.query.get_or_404(task_id)
     task.completed_at = None
     db.session.commit()
@@ -73,6 +133,15 @@ def incomplete_task(task_id):
 
 @goals_bp.route("/<goal_id>/tasks", methods=["POST"])
 def add_tasks(goal_id):
+    """
+    This route will add tasks to a goal.
+    ---
+    responses:
+        200:
+            description: Successfully added tasks to goal.
+        404:
+            description: Requested goal does not exist.
+    """
     goal = Goal.query.get_or_404(goal_id)
     request_body = request.get_json()
     goal.tasks = [Task.query.get(task_id) for task_id in request_body["task_ids"]]
@@ -82,6 +151,15 @@ def add_tasks(goal_id):
 
 @goals_bp.route("/<goal_id>/tasks", methods=["GET"])
 def goal_tasks(goal_id):
+    """
+    This route will retrieve tasks associated with a goal.
+    ---
+    responses:
+        200:
+            description: Successfully retrieved goal tasks.
+        404:
+            description: Requested goal does not exist.
+    """
     goal = Goal.query.get_or_404(goal_id)
     response = goal.to_dict()
     response["tasks"] = [task.to_dict() for task in goal.tasks]
