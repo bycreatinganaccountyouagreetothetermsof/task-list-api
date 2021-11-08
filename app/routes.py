7592e975d3839db368c970597196598129fa91a5
lain <bycreatinganaccountyouagreetothetermsof@users.noreply.github.com>
2021-11-07 16:50:44 -0800

    added /tasks/<task_id> route

--- app/routes.py
+++ app/routes.py
@@ -17,3 +17,22 @@ def handle_tasks():
         db.session.add(new_task)
         db.session.commit()
         return f"Task {new_task.title} successfully created", 201
+
+
+@tasks_bp.route("/<task_id>", methods=["GET", "PUT", "DELETE"])
+def handle_book(task_id):
+    task = Task.query.get(task_id)
+    if not task:
+        return f"Task {task_id} not found", 404
+    if request.method == "GET":
+        return {"task": task.to_dict()}
+    elif request.method == "PUT":
+        request_body = request.get_json()
+        task.title = request_body["title"]
+        task.description = request_body["description"]
+        db.session.commit()
+        return f"Task {task_id} successfully updated"
+    elif request.method == "DELETE":
+        db.session.delete(task)
+        db.session.commit()
+        return f"Task {task_id} successfully deleted"
